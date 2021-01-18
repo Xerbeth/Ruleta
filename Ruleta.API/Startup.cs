d0307654b72f4814244736d10caca32d5cf057b0
Xerbeth <faiber.torres.o@gmail.com>
2021-01-18 10:50:39 -0500

    Implementación Swagger

--- Ruleta.API/Startup.cs
+++ Ruleta.API/Startup.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -10,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using PAI.Domain.Services;
 using Ruleta.Domain.Services;
 using Ruleta.Domain.Services.Interfaces;
@@ -27,9 +30,8 @@ namespace Ruleta.API
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
+        {                      
             var connectionString = Configuration.GetConnectionString("DefaultConnection");
             services.AddScoped<IDocumentTypeServices, DocumentTypeServices>();
             services.AddScoped<IBetTypeServices, BetTypeServices>();
@@ -39,17 +41,33 @@ namespace Ruleta.API
             services.AddSingleton(Configuration);
 
             StartupServices.ConfigureServices(services, connectionString);
-            StartupTransactions.ConfigureServices(services, connectionString);
+            StartupTransactions.ConfigureServices(services, connectionString);           
+
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ROULETTE API", Version = "v1" });
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+                c.CustomSchemaIds(type => type.ToString());
+            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ROULETTE API V1");
+            });
 
             app.UseHttpsRedirection();
 
